e429c62d9e1f20fda394e9748a7e2d40622ec70c
CaiyueDamowang <www.caiyue@qq.com>
2020-09-30 12:59:33 +0800

    init frame

--- components/frame/index.js
+++ components/frame/index.js
@@ -1,23 +1,40 @@
 // components/frame/index.js
 Component({
-  /**
-   * 组件的属性列表
-   */
   properties: {
 
   },
 
-  /**
-   * 组件的初始数据
-   */
   data: {
-
+    statusBarHeight: 20,
   },
 
-  /**
-   * 组件的方法列表
-   */
   methods: {
+    getTabbarHeight() {
+      const promisify = (api) => {
+        return function (option) {
+          return new Promise((resolve, reject) => {
+            api({
+              ...option,
+              success: (result) => resolve(result),
+              fail: (reason) => reject(reason),
+            })
+          })
+        }
+      }
 
+      const getHeight = promisify(wx.getSystemInfo)
+      return getHeight().then(res => res.statusBarHeight)
+    }
+  },
+  lifetimes: {
+    attached() {
+      this.getTabbarHeight()
+        .then(statusBarHeight => {
+          statusBarHeight != this.data.statusBarHeight &&
+            this.setData({
+              statusBarHeight
+            })
+        })
+    },
   }
 })
