1471581c499099344fa1113d3a64ab4e518fe172
CaiyueDamowang <www.caiyue@qq.com>
2020-10-18 10:45:17 +0800

    fix bug

--- utils/extendPage.js
+++ utils/extendPage.js
@@ -2,12 +2,12 @@ const extendOptions = (options, expantion) => {
   return Object.assign(options, expantion);
 };
 
-const reduceMergeExpantions = (initialOptions, plugins) => {
+const mergePlugins = (initialOptions, plugins) => {
   const reducer = (context, plugin) => extendOptions(context, plugin)
   return plugins.reduce(reducer, initialOptions);
 };
 
-const _initLifetime = (plugin ,onload) => {
+const _initLifetime = (onload, plugin) => {
   return function(e) {      
     if (plugin) {
       extendOptions(this, plugin)
@@ -21,26 +21,12 @@ const _initLifetime = (plugin ,onload) => {
 
 const extendComponent = (plugins) => {
   const constructor = Component
-  const composePlugin = reduceMergeExpantions({}, plugins)
-  const notFuntcionPlugin = {}  // 组件扩展自定义属性必须在create周期声明
-
-  Object
-    .keys(composePlugin)
-    .forEach(key => {
-      const plugin = composePlugin[key]
-      if (typeof plugin !== 'function') {
-        notFuntcionPlugin[key] = plugin
-      }
-    })
+  const composePlugin = mergePlugins({}, plugins)
 
   return (options) => {
     // 组件样式隔离取消
     !options.options && (options.options = {})
     options.options.stylesIsolation = 'shared'
-    
-    // 添加扩展方法
-    !options.methods && (options.methods = {})
-    options.methods = extendOptions(options.methods, composePlugin)
 
     // 添加扩展属性
     const lifetimes = options.lifetimes  // !注意: 组件必须要有 lifetimes字段 可以为空
@@ -50,10 +36,10 @@ const extendComponent = (plugins) => {
     }
 
     if (!lifetimes.created){
-      lifetimes.created = _initLifetime(notFuntcionPlugin)
+      lifetimes.created = _initLifetime(null, composePlugin)
     } else {
       const create = lifetimes.created
-      lifetiems.created = _initLifetime(notFuntcionPlugin, create)
+      lifetiems.created = _initLifetime(create, composePlugin)
     }
 
     constructor(options)
@@ -62,7 +48,7 @@ const extendComponent = (plugins) => {
 
 const extendPage = (plugins) => {
   const constructor = Page
-  const composePulgin = reduceMergeExpantions({}, plugins)
+  const composePulgin = mergePlugins({}, plugins)
 
   return (options) => {
     const expandOptions = extendOptions(options,composePulgin)
